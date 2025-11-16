6c75d9ddc9b1dd9e33cff67f8b3bc1179145c56f
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-11-16 16:18:47 +0000

    template ajout_vedeur

--- ayimolou/settings.py
+++ ayimolou/settings.py
@@ -6,7 +6,7 @@ import cloudinary
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ayimolou-secret-key'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
